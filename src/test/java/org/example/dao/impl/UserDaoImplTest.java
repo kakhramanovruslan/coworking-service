fb1bb8e1af378ad5155ff724b834bec30a8e3b03
User <kakhramanov.ruslan@mail.ru>
2024-07-01 17:11:37 +0500

    lab2-v3 with tests

--- src/test/java/org/example/dao/impl/UserDaoImplTest.java
+++ src/test/java/org/example/dao/impl/UserDaoImplTest.java
@@ -3,6 +3,7 @@ package org.example.dao.impl;
 import org.example.config.ContainersEnvironment;
 import org.example.entity.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -30,15 +31,16 @@ public class UserDaoImplTest extends ContainersEnvironment {
     @Test
     public void testFindById(){
         Optional<User> foundUser = userDao.findById(testUser.getId());
+        Optional<User> notFoundPlayer = userDao.findById(999L);
+
         assertAll(
                 () -> assertTrue(foundUser.isPresent()),
                 () -> assertEquals(testUser.getId(), foundUser.get().getId()),
                 () -> assertEquals(testUser.getUsername(), foundUser.get().getUsername()),
-                () -> assertEquals(testUser.getPassword(), foundUser.get().getPassword())
+                () -> assertEquals(testUser.getPassword(), foundUser.get().getPassword()),
+                () -> assertFalse(notFoundPlayer.isPresent())
         );
 
-        Optional<User> notFoundPlayer = userDao.findById(999L);
-        assertFalse(notFoundPlayer.isPresent());
     }
 
     @Test
@@ -69,6 +71,43 @@ public class UserDaoImplTest extends ContainersEnvironment {
 
         assertEquals(userDao.findAll().size(), 2);
         assertFalse(userDao.findAll().isEmpty());
+    }
+
+    @Test
+    public void testSave(){
+        User testUser2 = User.builder()
+                .username("ruslan2")
+                .password("123")
+                .build();
+        User savedUser = userDao.save(testUser2);
+
+        assertAll(
+                () -> assertTrue(userDao.findById(savedUser.getId()).isPresent()),
+                () -> assertEquals(savedUser.getId(), testUser2.getId()),
+                () -> assertEquals(savedUser.getUsername(), testUser2.getUsername()),
+                () -> assertEquals(savedUser.getPassword(), testUser2.getPassword()),
+                () -> assertEquals(userDao.findAll().size(), 2)
+        );
+    }
+
+    @Test
+    @Disabled
+    public void testUpdate(){
+    }
+
+    @Test
+    public void testFindByUsername(){
+        Optional<User> foundUser = userDao.findByUsername(testUser.getUsername());
+        Optional<User> notFoundPlayer = userDao.findByUsername(" ");
+
+        assertAll(
+                () -> assertTrue(foundUser.isPresent()),
+                () -> assertEquals(testUser.getId(), foundUser.get().getId()),
+                () -> assertEquals(testUser.getUsername(), foundUser.get().getUsername()),
+                () -> assertEquals(testUser.getPassword(), foundUser.get().getPassword()),
+                () -> assertFalse(notFoundPlayer.isPresent())
+        );
+
     }
 
 }
--- src/test/java/org/example/dao/impl/WorkspaceDaoImplTest.java
+++ src/test/java/org/example/dao/impl/WorkspaceDaoImplTest.java
@@ -1,4 +1,123 @@
 package org.example.dao.impl;
 
-public class WorkspaceDaoImplTest {
+import org.example.config.ContainersEnvironment;
+import org.example.entity.Workspace;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class WorkspaceDaoImplTest extends ContainersEnvironment {
+
+    WorkspaceDaoImpl workspaceDao;
+    private Workspace testWorkspace;
+
+    @BeforeEach
+    public void setUp(){
+        workspaceDao = WorkspaceDaoImpl.getInstance();
+
+        workspaceDao.deleteAll();
+
+        testWorkspace = testWorkspace.builder()
+                .name("test-workspace-1")
+                .build();
+        workspaceDao.save(testWorkspace);
+    }
+
+    @Test
+    public void testFindAll(){
+        Workspace testWorkspace2 = Workspace.builder()
+                .name("test-workspace-2")
+                .build();
+
+        workspaceDao.save(testWorkspace2);
+
+        assertAll(
+            () -> assertThat(workspaceDao.findAll()).hasSize(2),
+            () -> assertThat(workspaceDao.findAll().isEmpty()).isFalse()
+        );
+    }
+
+    @Test
+    public void testFindById(){
+        Optional<Workspace> foundWorkspace = workspaceDao.findById(testWorkspace.getId());
+        Optional<Workspace> notFoundWorkspace = workspaceDao.findById(999L);
+        assertAll(
+                () -> assertTrue(foundWorkspace.isPresent()),
+                () -> assertThat(testWorkspace.getId()).isEqualTo(foundWorkspace.get().getId()),
+                () -> assertThat(testWorkspace.getName()).isEqualTo(foundWorkspace.get().getName()),
+                () -> assertFalse(notFoundWorkspace.isPresent())
+        );
+    }
+
+    @Test
+    public void testDeleteById(){
+        assertAll(
+                () -> assertThat(workspaceDao.deleteById(testWorkspace.getId())).isTrue(),
+                () -> assertThat(workspaceDao.findById(testWorkspace.getId()).isEmpty()).isTrue()
+        );
+    }
+
+    @Test
+    public void testDeleteAll(){
+        Workspace testWorkspace2 = Workspace.builder()
+                .name("test-workspace-2")
+                .build();
+        workspaceDao.save(testWorkspace2);
+
+        assertAll(
+                () -> assertThat(workspaceDao.deleteAll()).isTrue(),
+                () -> assertThat(workspaceDao.findAll().size()).isEqualTo(0)
+        );
+    }
+
+    @Test
+    public void testSave(){
+        Workspace testWorkspace2 = Workspace.builder()
+                .name("test-workspace-2")
+                .build();
+        Workspace savedWorkspace = workspaceDao.save(testWorkspace2);
+
+        assertAll(
+                () -> assertThat(workspaceDao.findById(savedWorkspace.getId()).isPresent()).isTrue(),
+                () -> assertThat(savedWorkspace.getId()).isEqualTo(testWorkspace2.getId()),
+                () -> assertThat(savedWorkspace.getName()).isEqualTo(testWorkspace2.getName()),
+                () -> assertThat(workspaceDao.findAll().size()).isEqualTo(2)
+        );
+    }
+
+    @Test
+    public void testUpdate(){
+        testWorkspace.setName("test-workspace-update");
+        workspaceDao.update(testWorkspace);
+
+        assertThat(workspaceDao.findById(testWorkspace.getId())).isPresent();
+        assertThat(workspaceDao.findById(testWorkspace.getId()).get().getName()).isEqualTo(testWorkspace.getName());
+    }
+
+    @Test
+    public void testFindByName(){
+        Optional<Workspace> foundWorkspace = workspaceDao.findByName(testWorkspace.getName());
+        Optional<Workspace> notFoundWorkspace = workspaceDao.findByName(" ");
+
+        assertAll(
+                () -> assertThat(foundWorkspace.isPresent()).isTrue(),
+                () -> assertThat(testWorkspace.getId()).isEqualTo(foundWorkspace.get().getId()),
+                () -> assertThat(testWorkspace.getName()).isEqualTo(foundWorkspace.get().getName()),
+                () -> assertThat(notFoundWorkspace.isPresent()).isFalse()
+        );
+    }
+
+    @Test
+    public void testDeleteByName(){
+        assertAll(
+                () -> assertThat(workspaceDao.deleteByName(testWorkspace.getName())).isTrue(),
+                () -> assertThat(workspaceDao.findByName(testWorkspace.getName()).isEmpty()).isTrue()
+        );
+    }
+
+
 }
